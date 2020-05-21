7419f8819eacb7a5c9d10049648c18e130ad038c
Tomasz Grande <grande.tomasz@gmail.com>
2020-05-21 20:35:41 +0200

    configure Google Tag Manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = {
   siteMetadata: {
@@ -14,5 +14,11 @@ module.exports = {
         apiToken: process.env.DATO_API_TOKEN,
       },
     },
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: process.env.GOOGLE_TAGMANAGER_ID
+      },
+    },
   ],
-}
+};
